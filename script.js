affc449939157345e40169004c2e991beea8d565
Erkezhan <e1344935@gmail.com>
2024-11-27 09:32:59 +0600

    array commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let f =["Ponaldu","Zidan","Neimar","Benzama"];
+
+f[1]="Messi";
+f.push("Pele","Gerrad");
+if(f.length>5){
+    console.log("kop");
+    
+}else{
+    console.log("az");
+    
+}
+console.log(f);

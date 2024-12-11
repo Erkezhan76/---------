2690114f4eb0d14e01b7a107e6b48a7bff8a1634
Erkezhan <e1344935@gmail.com>
2024-12-11 09:42:41 +0600

    intro to DOM

---
+++ hw3-week13/javascript/java/js.js
@@ -0,0 +1,11 @@
+let homeWork=document.querySelector("#hwork")
+homeWork.textContent="HW: Introduction to DOM"
+
+let name=document.getElementById("atyJony")
+name.textContent="Sharipkhan Erkezhan"
+
+let hobby=document.getElementsByClassName("hobby");
+
+hobby[0].innerHTML="<b>Coding</b>"
+hobby[1].innerHTML="<b>teaching</b>"
+hobby[2].innerHTML="<b>Reading</b>"
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let f =["Ponaldu","Zidan","Neimar","Benzama"];
 
 f[1]="Messi";
@@ -10,4 +11,16 @@ if(f.length>5){
     
 }
 console.log(f);
+*/
 
+let massiv = [1,2,3,4,5]
+alert (massiv)
+
+let kub = massiv.map(num=>num*num*num)
+
+console.log('кубталған сандар:' + kub);
+for(let massiv of kub){
+   let taq =kub.filter(num=>num%2!==0)
+   console.log('кубталған сандардың арасындағы тек тақ сандар:' + taq);
+}
+alert(kub)
